b2e6bc03a9181a60ae868a1c5b5d6b6f25b877b6
hyunS00 <optifun72@gmail.com>
2024-04-14 23:25:57 +0900

    식단 프로토타입

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
-
+from scrape_meal import get_weekly_dreamtower_meal
+import datetime as dt
 load_dotenv()
 SECRET_ENV = os.getenv("SECRET_ENV")
 client = WebClient(
@@ -10,7 +11,13 @@ client = WebClient(
 )
 
 try:
-    response = client.chat_postMessage(channel = "C06U0QC2TGE", text="WRITE_MESSAGE_HERE")
+    x = dt.datetime.now()
+    tmp = dt.datetime(2024, 4, 8, 15, 27, 4, 517207)
+    x.weekday()
+    today_meal = get_weekly_dreamtower_meal(tmp.weekday())
+    if today_meal:
+        response = client.chat_postMessage(channel = "C06U0QC2TGE", text=f'>*📅{today_meal["date"]}{today_meal["day"]}*\n>*점심 🥗*\n{today_meal["lunch"]}')
+        response = client.chat_postMessage(channel = "C06U0QC2TGE", text=f'>*저녁🍲*\n{today_meal["dinner"]}')
 
 except SlackApiError as err:
     print(f"Error:{err.response["error"]}")
---
+++ scrape_meal.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_weekly_dreamtower_meal(weekly):
+  url = "https://dorm.kyonggi.ac.kr:446/Khostel/mall_main.php?viewform=B0001_foodboard_list&gyear=2024&gmonth=04&gday=07"
+
+  response = requests.get(url)
+  print('응답코드: ', response.status_code)
+  response.raise_for_status()
+
+  html = response.content.decode('euc-kr')
+  soup = BeautifulSoup(html, 'html.parser')
+
+  # 경기드림타워 기숙사 메뉴 스크래핑
+  dream_tower = soup.select('table.boxstyle02 > tbody > tr')
+  # 일요일, 토요일 제거
+  del dream_tower[0]
+  del dream_tower[5]
+  today_meal = dream_tower[weekly]
+  #날짜 정보 가져오기
+  if today_meal.select_one('th > a') != None:
+    
+    date = today_meal.select_one('th > a').text.strip()
+    day = date.split()[1]
+    date = date.split()[0]
+    lunch_html = today_meal.select_one('td:nth-child(3)')
+  if lunch_html == None:
+    return False;
+  # 점심
+  if "미운영" in lunch_html.text.strip():
+    return False
+  lunch = today_meal.select_one('td:nth-child(3)').text.strip()
+  # 저녁
+  dinner = today_meal.select_one('td:nth-child(4)').text.strip()
+
+  dream_tower_meal = {
+    'lunch':lunch,
+    'dinner':dinner,
+    'date': date,
+    'day': day
+  }
+  print(dream_tower_meal)
+  return dream_tower_meal
+
+get_weekly_dreamtower_meal(0)
